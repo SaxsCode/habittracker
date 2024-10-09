f500bc061c47036aac612a717f5e5017e252ba79
SaxsCode <maxsax.sch@gmail.com>
2024-10-09 22:00:00 +0200

    feat: get habits tthrough json call

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,8 +1,31 @@
-function changeText() {
-    document.getElementById("tim").innerHTML = "changed text";
+
+document.addEventListener('DOMContentLoaded', function() { work() }, false);
+
+async function work() {
+
+  const habitsObject = await getHabits();
+  const habits = Array.isArray(habitsObject) ? habitsObject : Object.values(habitsObject)[0];
+
+  if(habits.length < 1) {
+    console.log("No habits found");
+    return;
   }
-  
-  // Wait for the DOM to be fully loaded before executing the function
-  document.addEventListener("DOMContentLoaded", function() {
-    changeText();
+
+  habits.forEach((habit, index) => {
+    console.log(`Habit ${index + 1}:`, habit.title);
   });
+   
+};
+
+async function getHabits() {
+  try {
+    const response = await fetch('./habits.json');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching habits:', error);
+    throw error;
+  }
+}
